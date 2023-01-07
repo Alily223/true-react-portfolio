c9d1852cb187572ea0f9a9d607ebe19851954296
Austin <112780937+Alily223@users.noreply.github.com>
2023-01-06 22:47:05 -0500

    updated api

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { Component, useEffect } from "react";
+import axios from "axios";
 
+export default class blog extends Component {
+  constructor() {
+    super();
 
-export default function(){
-    return(
-        <div>
-            <h1>Blog</h1>
-        </div>
-    )
+    this.state = {
+      blogItems: [],
+      isLoading: true,
+    };
+
+    this.getBlogItems = this.getBlogItems.bind(this);
+  }
+
+  getBlogItems() {
+    axios
+      .get("http://localhost:5000/api/data")
+      .then((response) => {
+        console.log("getting", response.data);
+        this.setState({
+          blogItems: response.data
+        });
+      })
+      .catch((error) => {
+        console.log("getBlogItems error", error);
+      });
+  }
+
+  componentDidMount() {
+    this.getBlogItems();
+  }
+
+  render() {
+    const blogRecords = this.state.blogItems.map(blogItem => {
+      return <div key={blogItem.id}>{blogItem.name} -- {blogItem.password}</div>
+    })
+    return (
+      <div>
+        {blogRecords}
+      </div>
+    );
+  }
 }
